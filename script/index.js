7875da5d872bb8d0a0001a212fdca1fab2b5f219
mrcomputer2008 <marcelo.ribeiro@proeja.com>
2020-04-21 14:19:28 -0300

    testes de configuracaoes e estilos

---
+++ script/index.js
@@ -0,0 +1,23 @@
+// codigos funcionais e testados
+// Nao sao carregados no site
+let nome = "Hello World";
+console.log(nome);
+var num1 = 1;
+var num2 = 2;
+soma = num1 + num2;
+console.log(nome + " = " + soma);
+// criando uma funcao
+/* var somar2 = function(num3,num4)
+{
+    return num3 + num4
+}*/
+// definindo os valores para o calculo
+// var somar2 = (num3,num4) => num3+num4;
+// declaracao de string
+const nome2 = 'luiz';
+const sobreNome = 'Duarte';
+// contando a string nome3
+let nome3 = "Marcelo";
+console.log(nome3.length);
+// Concatenacao de strings
+console.log(nome2 +" " + sobreNome);
